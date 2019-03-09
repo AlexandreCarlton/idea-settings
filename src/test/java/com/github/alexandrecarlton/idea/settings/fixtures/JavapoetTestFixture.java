56123a7d6776759e915b5f9e4b108b0456980163
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2019-03-09 14:05:21 +1100

    Extract tests into "framework" using Junit suites
    This will allow us to drop in a class and have bazel test this for us.
    The problem is that we're just running one target, with no hint of
    progress until everything is completed. Luckily, we still get separate
    data in our test cases.
    
    In future, we'll probably run with individual java_test invocations,
    albeit using a custom rule to alleviate the boilerplate needed.

---
+++ src/test/java/com/github/alexandrecarlton/idea/settings/fixtures/JavapoetTestFixture.java
@@ -0,0 +1,40 @@
+package com.github.alexandrecarlton.idea.settings.fixtures;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * A base test class to set up Javapoet for settings configuration.
+ */
+public abstract class JavapoetTestFixture {
+
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+  protected Path javapoet;
+
+  @Before
+  public void setUp() throws IOException {
+    final String javapoetPomLocation = requireNonNull(System.getProperty("javapoet.pom"));
+    final Path originalJavaPoet = Paths.get(javapoetPomLocation).getParent();
+    javapoet = temporaryFolder.newFolder().toPath().resolve("javapoet");
+    Files
+        .walk(originalJavaPoet)
+        .forEach(source -> {
+          try {
+            Files.copy(source, javapoet.resolve(originalJavaPoet.relativize(source)));
+          } catch (IOException e) {
+            throw new UncheckedIOException(e);
+          }
+        });
+  }
+}
---
+++ src/test/java/com/github/alexandrecarlton/idea/settings/tests/AllTests.java
@@ -0,0 +1,12 @@
+package com.github.alexandrecarlton.idea.settings.tests;
+
+import com.github.alexandrecarlton.idea.settings.applier.impl.editor.codestyle.java.JavaImportSettingsApplierTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    JavaImportSettingsApplierTest.class,
+})
+public class AllTests {}
