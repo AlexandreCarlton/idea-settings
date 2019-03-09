379d240d96063769b55b8eae1b3e967ca00b5aa8
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2019-03-10 00:42:10 +1100

    Add tests for Java auto import settings

--- BUILD
+++ BUILD
@@ -9,6 +9,7 @@ test_suite(
         "//src/test/java/com/github/alexandrecarlton/idea/settings/applier/impl/build_execution_deployment",
         "//src/test/java/com/github/alexandrecarlton/idea/settings/applier/impl/build_execution_deployment/compiler",
         "//src/test/java/com/github/alexandrecarlton/idea/settings/applier/impl/editor/codestyle/java",
+        "//src/test/java/com/github/alexandrecarlton/idea/settings/applier/impl/editor/general/auto_import",
         "//src/test/java/com/github/alexandrecarlton/idea/settings/applier/impl/other_settings/checkstyle",
     ],
 )
---
+++ src/test/java/com/github/alexandrecarlton/idea/settings/applier/impl/editor/general/auto_import/JavaAutoImportSettingsApplierTest.java
@@ -0,0 +1,29 @@
+package com.github.alexandrecarlton.idea.settings.applier.impl.editor.general.auto_import;
+
+import com.github.alexandrecarlton.idea.settings.fixtures.JavapoetTestFixture;
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JavaAutoImportSettingsApplierTest extends JavapoetTestFixture {
+
+  @Test
+  public void testExcludeFromImportAndCompletion() throws Exception {
+    writeIdeaSettingsFile(
+        "editor:",
+        "  general:",
+        "    autoImport:",
+        "      java:",
+        "        excludeFromImportAndCompletion:",
+        "          - com.google.inject.Inject",
+        "          - com.sun.istack.internal.Nullable");
+    runIdeaSettings();
+    // Xmlunit does not appear to support asList() in the way one would expect.
+    assertThatXml(".idea/codeInsightSettings.xml")
+        .hasXPath("//component[@name='JavaProjectCodeInsightSettings']/excluded-names/name[text()='com.google.inject.Inject']");
+    assertThatXml(".idea/codeInsightSettings.xml")
+        .hasXPath("//component[@name='JavaProjectCodeInsightSettings']/excluded-names/name[text()='com.sun.istack.internal.Nullable']");
+  }
+
+}
