75e87cb12e73b554f89dc5e8259bdee579a91805
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2019-03-09 21:50:52 +1100

    Add versions to requiredPlugins test

--- src/test/java/com/github/alexandrecarlton/idea/settings/applier/impl/build_execution_deployment/BuildExecutionDeploymentSettingsApplierTest.java
+++ src/test/java/com/github/alexandrecarlton/idea/settings/applier/impl/build_execution_deployment/BuildExecutionDeploymentSettingsApplierTest.java
@@ -18,4 +18,21 @@ public class BuildExecutionDeploymentSettingsApplierTest extends JavapoetTestFix
         .hasXPath("//component[@name='ExternalDependencies']/plugin[@id='CheckStyle-IDEA']");
   }
 
+  @Test
+  public void testRequiredPluginsWithVersion() throws Exception {
+    writeIdeaSettingsFile(
+        "buildExecutionDeployment:",
+        "  requiredPlugins:",
+        "    - plugin: CheckStyle-IDEA",
+        "      minimumVersion: 5.23.0",
+        "      maximumVersion: 5.24.1");
+    runIdeaSettings();
+    assertThatXml(".idea/externalDependencies.xml")
+        .valueByXPath("//component[@name='ExternalDependencies']/plugin[@id='CheckStyle-IDEA']/@min-version")
+        .isEqualTo("5.23.0");
+    assertThatXml(".idea/externalDependencies.xml")
+        .valueByXPath("//component[@name='ExternalDependencies']/plugin[@id='CheckStyle-IDEA']/@max-version")
+        .isEqualTo("5.24.1");
+  }
+
 }
