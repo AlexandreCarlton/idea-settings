18c6f2f366fddf02e1428a94d04ff78b90105570
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2019-11-24 17:46:39 +1100

    Convert Build,Execution,Deployment tests to Kotlin

---
+++ src/test/kotlin/com/github/alexandrecarlton/idea/settings/applier/impl/build_execution_deployment/BuildExecutionDeploymentSettingsApplierTest.kt
@@ -0,0 +1,77 @@
+package com.github.alexandrecarlton.idea.settings.applier.impl.build_execution_deployment
+
+import org.assertj.core.api.Assertions.assertThat
+import org.mockito.Mockito.verify
+
+import com.github.alexandrecarlton.idea.settings.applier.api.SettingsApplier
+import com.github.alexandrecarlton.idea.settings.fixtures.IdeaSettingsTestFixture
+import com.github.alexandrecarlton.idea.settings.layout.build_execution_deployment.BuildExecutionDeploymentSettings
+import com.github.alexandrecarlton.idea.settings.layout.build_execution_deployment.ImmutableBuildExecutionDeploymentSettings
+import com.github.alexandrecarlton.idea.settings.layout.build_execution_deployment.ImmutableRequiredPlugin
+import com.github.alexandrecarlton.idea.settings.layout.build_execution_deployment.build_tools.BuildToolsSettings
+import com.github.alexandrecarlton.idea.settings.layout.build_execution_deployment.build_tools.ImmutableBuildToolsSettings
+import com.github.alexandrecarlton.idea.settings.layout.build_execution_deployment.compiler.CompilerSettings
+import com.github.alexandrecarlton.idea.settings.layout.build_execution_deployment.compiler.ImmutableCompilerSettings
+import com.intellij.externalDependencies.DependencyOnPlugin
+import com.intellij.externalDependencies.ExternalDependenciesManager
+import java.util.Collections
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+
+class BuildExecutionDeploymentSettingsApplierTest : IdeaSettingsTestFixture() {
+
+    private lateinit var settingsApplier: SettingsApplier<BuildExecutionDeploymentSettings>
+    private lateinit var externalDependenciesManager: ExternalDependenciesManager
+    @Mock
+    private lateinit var buildToolsSettingsApplier: SettingsApplier<BuildToolsSettings>
+    @Mock
+    private lateinit var compilerSettingsApplier: SettingsApplier<CompilerSettings>
+
+    @Before
+    public override fun setUp() {
+        externalDependenciesManager = ExternalDependenciesManager.getInstance(project)
+        settingsApplier = BuildExecutionDeploymentSettingsApplier(externalDependenciesManager, buildToolsSettingsApplier, compilerSettingsApplier)
+    }
+
+    @After
+    public override fun tearDown() {
+        externalDependenciesManager.allDependencies = emptyList()
+    }
+
+    @Test
+    fun requiredPluginsApplied() {
+        settingsApplier.apply(ImmutableBuildExecutionDeploymentSettings.builder()
+                .addRequiredPlugins(ImmutableRequiredPlugin.builder()
+                        .plugin("CheckStyle-IDEA")
+                        .build())
+                .build())
+        assertThat(externalDependenciesManager.allDependencies)
+                .containsOnly(DependencyOnPlugin("CheckStyle-IDEA", null, null))
+    }
+
+    @Test
+    fun requiredPluginsWithVersionApplied() {
+        settingsApplier.apply(ImmutableBuildExecutionDeploymentSettings.builder()
+                .addRequiredPlugins(ImmutableRequiredPlugin.builder()
+                        .plugin("CheckStyle-IDEA")
+                        .minimumVersion("5.23.0")
+                        .maximumVersion("5.24.1")
+                        .build())
+                .build())
+        assertThat(externalDependenciesManager.allDependencies)
+                .containsOnly(DependencyOnPlugin("CheckStyle-IDEA", "5.23.0", "5.24.1"))
+    }
+
+    @Test
+    fun childSettingsApplied() {
+        settingsApplier.apply(ImmutableBuildExecutionDeploymentSettings.builder()
+                .compiler(ImmutableCompilerSettings.builder().build())
+                .buildTools(ImmutableBuildToolsSettings.builder().build())
+                .build())
+        verify(buildToolsSettingsApplier).apply(ImmutableBuildToolsSettings.builder().build())
+        verify(compilerSettingsApplier).apply(ImmutableCompilerSettings.builder().build())
+    }
+
+}
---
+++ src/test/kotlin/com/github/alexandrecarlton/idea/settings/applier/impl/build_execution_deployment/compiler/CompilerSettingsApplierTest.kt
@@ -0,0 +1,87 @@
+package com.github.alexandrecarlton.idea.settings.applier.impl.build_execution_deployment.compiler
+
+import com.github.alexandrecarlton.idea.settings.applier.api.SettingsApplier
+import com.github.alexandrecarlton.idea.settings.fixtures.IdeaSettingsTestFixture
+import com.github.alexandrecarlton.idea.settings.layout.build_execution_deployment.compiler.CompilerSettings
+import com.github.alexandrecarlton.idea.settings.layout.build_execution_deployment.compiler.ImmutableCompilerSettings
+import com.google.common.collect.ImmutableList
+import com.intellij.compiler.CompilerConfiguration
+import com.intellij.compiler.CompilerConfigurationImpl
+import com.intellij.compiler.CompilerWorkspaceConfiguration
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class CompilerSettingsApplierTest : IdeaSettingsTestFixture() {
+
+    private lateinit var settingsApplier: SettingsApplier<CompilerSettings>
+    private lateinit var compilerConfiguration: CompilerConfiguration
+    private lateinit var compilerWorkspaceConfiguration: CompilerWorkspaceConfiguration
+
+    @Before
+    public override fun setUp() {
+        compilerConfiguration = CompilerConfiguration.getInstance(project)
+        compilerWorkspaceConfiguration = CompilerWorkspaceConfiguration.getInstance(project)
+        settingsApplier = CompilerSettingsApplier(compilerConfiguration, compilerWorkspaceConfiguration)
+    }
+
+
+    @Test
+    fun addRuntimeAssertionsForNotnullAnnotatedMethodsAndParametersApplied() {
+        settingsApplier.apply(ImmutableCompilerSettings.builder()
+                .addRuntimeAssertionsForNotnullAnnotatedMethodsAndParameters(false)
+                .build())
+        assertThat(compilerConfiguration.isAddNotNullAssertions).isFalse
+    }
+
+    @Test
+    fun buildProcessHeapSizeApplied() {
+        settingsApplier.apply(ImmutableCompilerSettings.builder()
+                .buildProcessHeapSizeMbytes(1234)
+                .build())
+        assertThat(compilerConfiguration.getBuildProcessHeapSize(0)).isEqualTo(1234)
+    }
+
+    @Test
+    fun compileIndependentModulesInParallel() {
+        settingsApplier.apply(ImmutableCompilerSettings.builder()
+                .compileIndependentModulesInParallel(true)
+                .build())
+        assertThat(compilerWorkspaceConfiguration.PARALLEL_COMPILATION).isTrue
+    }
+
+    @Test
+    fun rebuildModuleOnDependencyChange() {
+        settingsApplier.apply(ImmutableCompilerSettings.builder()
+                .rebuildModuleOnDependencyChange(false)
+                .build())
+        assertThat(compilerWorkspaceConfiguration.REBUILD_ON_DEPENDENCY_CHANGE).isFalse
+    }
+
+    @Test
+    fun sharedBuildProcessVmOptions() {
+        settingsApplier.apply(ImmutableCompilerSettings.builder()
+                .sharedBuildProcessVmOptions("-Xms1g")
+                .build())
+        assertThat(compilerConfiguration.buildProcessVMOptions).isEqualTo("-Xms1g")
+    }
+
+    @Test
+    fun resourcePatternsIfNotSetAreDefaults() {
+        settingsApplier.apply(ImmutableCompilerSettings.builder()
+                .build())
+        assertThat(compilerConfiguration).isInstanceOf(CompilerConfigurationImpl::class.java)
+        val compilerConfigurationImpl = compilerConfiguration as CompilerConfigurationImpl
+        assertThat(compilerConfigurationImpl.resourceFilePatterns).contains("!?*.java")
+    }
+
+    @Test
+    fun resourcePatterns() {
+        settingsApplier.apply(ImmutableCompilerSettings.builder()
+                .resourcePatterns(ImmutableList.of("resource.properties"))
+                .build())
+        assertThat(compilerConfiguration).isInstanceOf(CompilerConfigurationImpl::class.java)
+        val compilerConfigurationImpl = compilerConfiguration as CompilerConfigurationImpl
+        assertThat(compilerConfigurationImpl.resourceFilePatterns).containsExactly("resource.properties")
+    }
+}
