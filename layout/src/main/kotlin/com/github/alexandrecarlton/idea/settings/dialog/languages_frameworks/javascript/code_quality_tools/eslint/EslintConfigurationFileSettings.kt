bc0bcc52bc3fcd0a462979387cfe58b4e20c1982
Alexandre Carlton <alexandre.nikolei.carlton@gmail.com>
2020-03-28 19:04:57 +1100

    Use @get:JsonValue for "inline" sealed classes
    This works out really well.

--- layout/src/main/kotlin/com/github/alexandrecarlton/idea/settings/dialog/languages_frameworks/javascript/code_quality_tools/eslint/EslintConfigurationFileSettings.kt
+++ layout/src/main/kotlin/com/github/alexandrecarlton/idea/settings/dialog/languages_frameworks/javascript/code_quality_tools/eslint/EslintConfigurationFileSettings.kt
@@ -3,6 +3,7 @@ package com.github.alexandrecarlton.idea.settings.dialog.languages_frameworks.ja
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.annotation.JsonTypeName
+import com.fasterxml.jackson.annotation.JsonValue
 import java.io.File
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
@@ -11,14 +12,8 @@ sealed class EslintConfigurationFileSettings
 @JsonTypeName("Automatic Search")
 object EslintConfigurationFileSettingsAutomaticSearch : EslintConfigurationFileSettings()
 
-/**
- * Inline classes aren't supported as being:
- * - sealed (by Kotlin)
- * - unwrapped (by Jackson)
- * As such, we have an extra 'file' attribute we need to set.
- */
 @JsonTypeName("Configuration file")
 data class EslintConfigurationFileSettingsConfigurationFile(
-    @JsonProperty("File")
+    @get:JsonValue
     val file: File? = null
 ) : EslintConfigurationFileSettings()
